6ad43cd9ddd30d04a73a388bfe788b9115493c41
BlueColour2589 <bluecolour112@gmail.com>
2025-08-12 04:34:02 +0000

    Create Greeting.tsx

---
+++ frontend/components/Header/Greeting.tsx
@@ -0,0 +1,12 @@
+// components/Header/Greeting.tsx
+import { FC } from 'react';
+
+const Greeting: FC = () => {
+  return (
+    <div className="text-2xl font-semibold text-gray-800 dark:text-gray-100">
+      Good morning, Celestina White & Mark Peters
+    </div>
+  );
+};
+
+export default Greeting;
