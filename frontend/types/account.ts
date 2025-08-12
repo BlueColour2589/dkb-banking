e6af127448a383cc1b9330a68eb7088cbb035d0c
BlueColour2589 <bluecolour112@gmail.com>
2025-08-12 05:35:43 +0000

    Delete frontend/types directory

--- frontend/types/account.ts
+++
@@ -1,9 +0,0 @@
-// types/account.ts
-export interface Account {
-  id: string;
-  owners: string[]; // e.g., ['Celestina White', 'Mark Peters']
-  balance: number;
-  currency: string;
-  incomeChange: number;
-  createdAt: string;
-}
--- frontend/types/transaction.ts
+++
@@ -1,9 +0,0 @@
-// types/transaction.ts
-export interface Transaction {
-  id: string;
-  date: string;
-  description: string;
-  amount: number;
-  currency: string;
-  recipient: string;
-}
