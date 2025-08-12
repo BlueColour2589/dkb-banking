70000660945464fbdb05eada92211bc8ff48a5dd
BlueColour2589 <bluecolour112@gmail.com>
2025-08-12 04:34:34 +0000

    Create Notifications.tsx

---
+++ frontend/components/Header/Notifications.tsx
@@ -0,0 +1,12 @@
+// components/Header/Notifications.tsx
+import { FC } from 'react';
+
+const Notifications: FC = () => {
+  return (
+    <div className="text-sm text-gray-500 dark:text-gray-400">
+      At a glance summary of your account. No new alerts.
+    </div>
+  );
+};
+
+export default Notifications;
