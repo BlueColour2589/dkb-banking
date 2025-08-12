241576b018f770387840796fb7a6246634ca37d2
BlueColour2589 <bluecolour112@gmail.com>
2025-08-12 09:48:39 +0000

    Update Greeting.tsx

--- frontend/components/Header/Greeting.tsx
+++ frontend/components/Header/Greeting.tsx
@@ -3,14 +3,13 @@ import { FC } from 'react';
 const Greeting: FC = () => {
   const hour = new Date().getHours();
   let greeting = 'Good morning';
-
   if (hour >= 12 && hour < 18) greeting = 'Good afternoon';
   else if (hour >= 18) greeting = 'Good evening';
 
   return (
-    <div className="text-2xl font-semibold text-gray-800 dark:text-gray-100">
-      {greeting}, Celestina White & Mark Peters
-    </div>
+    <h2 className="text-4xl font-bold text-blue-600 mb-8">
+      {greeting}, Celestina White & Mark Peters!
+    </h2>
   );
 };
 
